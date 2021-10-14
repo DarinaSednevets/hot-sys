a2b694ecd6ae1753a00cf07ff620099e87f5ee19
DarinaSednevets <dariaausten@gmail.com>
2021-10-14 17:41:06 +0300

    add bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { React } from 'react';
+import './App.scss';
+
+const App = () => {
+
+  return (
+    <>
+      <h1>Luftkondensierte
+        Kältemaschinen
+        & Kaltwassersätze</h1>
+    </>
+  )
+}
+
+export default App;
