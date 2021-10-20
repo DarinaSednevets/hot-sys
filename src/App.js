8e42e947c59644379f9849b5bd5e13525879e9fd
DarinaSednevets <dariaausten@gmail.com>
2021-10-20 21:39:41 +0300

    apointment/ presentation for m & d

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { React } from 'react';
 import './App.scss';
 
 
+
 const App = () => {
 
   return (
@@ -11,7 +12,12 @@ const App = () => {
         <nav class="  navbar navbar-expand-lg navbar-light  ">
           <div class="container-fluid">
 
-            <a class="navbar-brand" href="/index.html">logo</a>
+            <a class="navbar-brand" href="/index.html" >
+              <picture className="logo">
+                <source media="(min-width: 1400px)" srcset="./images/logo-d.png"></source>
+                <img src="./images/logo-m.png" alt="logo" />
+              </picture>
+            </a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
@@ -79,18 +85,25 @@ const App = () => {
       </header>
 
       <section className="presentation">
-        <h1 class="h1">
-          Luftkondensierte<br />
-          Kältemaschinen<br />
-          & Kaltwassersätze<br />
-        </h1>
-        <div className="decoration-line"></div>
-        <div class="presentation-img-1"></div>
-        <p className="text">Aufgrund der kompakten Bauart
-          werden platzsparende luftkondensierte Kompressionskältemaschinen
-          (umgangsspr. „Kaltwassersätze“)
-          vielseitig zur Prozesskühlung und Gebäudeklimatisierung eingesetzt.
-        </p>
+        <div className="presentation-text-d">
+          <h1 class="h1">
+            Luftkondensierte<br />
+            Kältemaschinen<br />
+            & Kaltwassersätze<br />
+          </h1>
+          <div className="decoration-line"></div>
+          <div className="decoration-line-d"></div>
+          <div class="presentation-img-1-m"></div>
+          <p className="text">Aufgrund der kompakten Bauart
+            werden platzsparende luftkondensierte Kompressionskältemaschinen
+            (umgangsspr. „Kaltwassersätze“)
+            vielseitig zur Prozesskühlung und Gebäudeklimatisierung eingesetzt.
+          </p>
+          <div className="decoration-line-d"></div>
+        </div>
+
+        <div className="presentation-img-1-d"></div>
+
         <div class="presentation-img-2"></div>
       </section>
 
@@ -136,16 +149,37 @@ const App = () => {
       <section className="advantages">
         <h2 className="h2">IHRE VORTEILE</h2>
         <ul className="advantages-list list" >
-          <li className="item">Geringe Stellfläche</li>
-          <li className="item">Mit und ohne integrierter Freikühlung</li>
-          <li className="item">Für Außentemperaturen bis +55°C</li>
-          <li className="item">Für Außen- und Innenaufstellung erhältlich</li>
-          <li className="item">Betriebsbereit bei Anlieferung</li>
-          <li className="item">Automatische Freikühlschaltung für verringerten Energieverbrauch</li>
-          <li className="item">Robustes Gehäuse für Außenaufstellung</li>
-          <li className="item">Geringe Schallemissionen</li>
-          <li className="item">Wetterfest und Langlebig</li>
-          <li className="item">Erhöhte Betriebssicherheit</li>
+
+          <li className="item">
+            <div className="icon">
+              <svg viewBox="0 0 40 32" width="49" height="56">
+                <path stroke-linejoin="round" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="0.8205" d="M14.724 0.821c0-0.227-0.184-0.41-0.41-0.41h-13.493c-0.227 0-0.41 0.184-0.41 0.41v8.096c0 0.227 0.184 0.41 0.41 0.41h2.699c0.227 0 0.41-0.184 0.41-0.41v-4.987h10.384c0.227 0 0.41-0.184 0.41-0.41v-2.699z"></path>
+                <path stroke-linejoin="round" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="0.8205" d="M25.108 0.41c-0.227 0-0.41 0.184-0.41 0.41v2.699c0 0.227 0.184 0.41 0.41 0.41h10.384v4.987c0 0.227 0.184 0.41 0.41 0.41h2.699c0.227 0 0.41-0.184 0.41-0.41v-8.096c0-0.227-0.184-0.41-0.41-0.41h-13.493z"></path>
+                <path stroke-linejoin="round" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="0.8205" d="M14.313 31.59c0.227 0 0.41-0.184 0.41-0.41v-2.698c0-0.227-0.184-0.41-0.41-0.41h-10.384v-4.987c0-0.227-0.184-0.41-0.41-0.41h-2.699c-0.227 0-0.41 0.184-0.41 0.41v8.096c0 0.227 0.184 0.41 0.41 0.41h13.493z"></path>
+                <path stroke-linejoin="round" stroke-linecap="butt" stroke-miterlimit="4" stroke-width="0.8205" d="M24.697 31.179c0 0.227 0.184 0.41 0.41 0.41h13.493c0.227 0 0.41-0.184 0.41-0.41v-8.096c0-0.227-0.184-0.41-0.41-0.41h-2.699c-0.227 0-0.41 0.184-0.41 0.41v4.987h-10.384c-0.227 0-0.41 0.184-0.41 0.41v2.698z"></path>
+              </svg>
+            </div>
+            <p>Geringe Stellfläche</p>
+          </li>
+
+          <li className="item">
+            <div className="icon">
+              <svg viewBox="0 0 29 32" width="49" height="56">
+                <path d="M27.621 22.2l-2.46-1.438 2.14-0.581c0.527-0.144 0.841-0.694 0.697-1.225l-0.257-0.969c-0.138-0.531-0.684-0.85-1.211-0.706l-5.002 1.356-4.506-2.637 4.512-2.637 5.002 1.356c0.527 0.144 1.067-0.175 1.211-0.706l0.257-0.969c0.138-0.531-0.169-1.081-0.697-1.225l-2.14-0.581 2.46-1.438c0.471-0.275 0.634-0.887 0.364-1.369l-0.496-0.869c-0.27-0.481-0.879-0.644-1.349-0.369l-2.46 1.438 0.571-2.169c0.138-0.531-0.169-1.081-0.697-1.225l-0.954-0.256c-0.527-0.144-1.067 0.175-1.211 0.706l-1.337 5.062-4.512 2.637v-5.281l3.659-3.706c0.383-0.388 0.383-1.025 0-1.413l-0.697-0.706c-0.383-0.387-1.010-0.387-1.393 0l-1.563 1.587v-2.869c0-0.55-0.439-1-0.985-1h-0.985c-0.546 0-0.985 0.45-0.985 1v2.881l-1.563-1.587c-0.383-0.387-1.010-0.387-1.393 0l-0.722 0.706c-0.383 0.388-0.383 1.025 0 1.412l3.659 3.706v5.281l-4.512-2.637-1.337-5.063c-0.138-0.531-0.684-0.85-1.211-0.706l-0.954 0.256c-0.527 0.144-0.841 0.694-0.697 1.225l0.571 2.169-2.46-1.438c-0.471-0.275-1.073-0.112-1.349 0.369l-0.496 0.869c-0.27 0.481-0.113 1.088 0.364 1.369l2.46 1.438-2.14 0.569c-0.527 0.144-0.841 0.694-0.697 1.225l0.257 0.969c0.138 0.531 0.684 0.85 1.211 0.706l5.002-1.356 4.512 2.638-4.512 2.637-5.002-1.356c-0.527-0.144-1.067 0.175-1.211 0.706l-0.257 0.969c-0.138 0.531 0.169 1.081 0.697 1.225l2.14 0.581-2.46 1.438c-0.471 0.275-0.634 0.887-0.364 1.369l0.496 0.869c0.27 0.481 0.879 0.644 1.349 0.369l2.46-1.438-0.571 2.169c-0.138 0.531 0.169 1.081 0.697 1.225l0.954 0.256c0.527 0.144 1.067-0.175 1.211-0.706l1.337-5.063 4.512-2.637v5.281l-3.659 3.706c-0.383 0.387-0.383 1.025 0 1.413l0.697 0.706c0.383 0.387 1.010 0.387 1.393 0l1.563-1.587v2.869c0 0.55 0.439 1 0.985 1h0.985c0.546 0 0.985-0.45 0.985-1v-2.881l1.563 1.587c0.383 0.388 1.010 0.388 1.393 0l0.697-0.706c0.383-0.387 0.383-1.025 0-1.413l-3.659-3.706v-5.281l4.512 2.637 1.337 5.063c0.138 0.531 0.684 0.85 1.211 0.706l0.954-0.256c0.527-0.144 0.841-0.694 0.697-1.225l-0.571-2.169 2.46 1.438c0.471 0.275 1.073 0.112 1.349-0.369l0.496-0.869c0.289-0.469 0.132-1.081-0.345-1.356z"></path>
+              </svg>
+            </div>
+            <p>Mit und ohne integrierter Freikühlung</p>
+          </li>
+
+
+          <li className="item"><p>Für Außentemperaturen bis +55°C</p></li>
+          <li className="item"><p>Für Außen- und Innenaufstellung erhältlich</p></li>
+          <li className="item"><p>Betriebsbereit bei Anlieferung</p></li>
+          <li className="item"><p>Automatische Freikühlschaltung für verringerten Energieverbrauch</p></li>
+          <li className="item"><p>Robustes Gehäuse für Außenaufstellung</p></li>
+          <li className="item"><p>Geringe Schallemissionen</p></li>
+          <li className="item"><p>Wetterfest und Langlebig</p></li>
+          <li className="item"><p>Erhöhte Betriebssicherheit</p></li>
         </ul>
 
       </section>
@@ -217,16 +251,20 @@ const App = () => {
         </ul>
       </section>
 
-      <sectiion class="apointment">
+      <section className="apointment">
+        <div className="logo-gray"></div>
         <h2>Jetzt Termin vereinbaren!</h2>
-        <p>Gerne stehen wir Ihnen für eine individuelle Beratung telefonisch
-          und vor Ort zur
-          Verfügung und freuen
-          uns über Ihre Kontaktaufnahme. Sprechen Sie uns an!
-          Fragen Sie nach einem Rückruf oder kontaktieren Sie uns unter: info@elsasser-technik.de
-        </p>
-        <h2>Kontakt</h2>
-      </sectiion>
+        <div className="apointment-div-d">
+          <p>Gerne stehen wir Ihnen für eine individuelle Beratung telefonisch
+            und vor Ort zur
+            Verfügung und freuen
+            uns über Ihre Kontaktaufnahme. Sprechen Sie uns an!<br></br><br></br>
+            Fragen Sie nach einem Rückruf oder kontaktieren Sie uns unter:
+            <a class=" mail" href="mailto:info@elsasser-technik.de">info@elsasser-technik.de</a>
+          </p>
+          <div className="kontakt">Kontakt</div>
+        </div>
+      </section>
 
       <footer class="footer">
         <adress>
@@ -241,7 +279,7 @@ const App = () => {
               <a class=" tel" href="tel:+49 (0) 211 9232 3052">+49 (0) 211 9232 3052</a>
               <br></br>
               <span class="adress-subtitle">E-Mail:</span>
-              <a class=" mail" href="mailto:info@elsasser-technik.de">info@elsasser-technik.de</a>
+              <a className="mail" href="mailto:info@elsasser-technik.de">info@elsasser-technik.de</a>
             </li>
             <li class="footer-item">
               <p class="adress-title">Shortlinks</p>
