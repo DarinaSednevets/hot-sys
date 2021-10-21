04ed57b8b7c2a1ea580e0abee6f543304e8b0f2d
DarinaSednevets <dariaausten@gmail.com>
2021-10-21 15:01:15 +0300

    a little bit fix

--- src/Content.js
+++ src/Content.js
@@ -7,7 +7,7 @@ const Content = () => {
                 <nav class="  navbar navbar-expand-md navbar-light  ">
                     <div class="container-fluid">
                         <a class="navbar-brand" href="/index.html" >
-                            <div class="logo-color"></div>
+                            <div className="logo-color"></div>
                         </a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
